8e6d18094f6ba165d1f0d4cd558699de8cdae056
Nathan Hietala <Nrhietala@gmail.com>
2021-02-11 11:31:44 -0600

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 function url(path) {
   return process.env.NODE_ENV === "development"
     ? `http:localhost:5000${path}`
-    : "";
+    : path;
 }
 // development
 // production
